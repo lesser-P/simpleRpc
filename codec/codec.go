550944bb9d4323a55b6a8aec3ff9b6b94908687c
lesser-P <1275830377@qq.com>
2024-02-28 16:52:11 +0800

    init

---
+++ codec/codec.go
@@ -0,0 +1,33 @@
+package codec
+
+import "io"
+
+type Header struct {
+	ServiceMethod string
+	Seq           uint64 //请求的序列号
+	Error         string
+}
+
+// 实现不同的Codec实例
+type Codec interface {
+	io.Closer
+	ReadHeader(header *Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module simpleRpc
+
+go 1.21
